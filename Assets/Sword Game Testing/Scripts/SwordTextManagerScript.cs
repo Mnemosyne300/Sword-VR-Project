b574a690a21faacfaa9a8eece404d94f8692efd0
Jonathan Ramos <jramos1@fullsail.edu>
2016-07-29 19:12:26 -0400

    Added rest of the new Assets

---
+++ Assets/Sword Game Testing/Scripts/SwordTextManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordTextManagerScript : MonoBehaviour {
+
+    GameObject HMD;
+
+    Transform[] Texts;
+
+    private int HitCounts = 0;
+
+
+    // Use this for initialization
+    void Start () {
+        HMD = GameObject.FindGameObjectWithTag("MainCamera");
+        Texts = GetComponentsInChildren<Transform>();
+
+        if (Texts[1].tag == "HitText")
+        {
+            Texts[1].GetComponent<TextMesh>().text = "Hits : " + HitCounts.ToString();
+        }
+        else
+            Debug.Log("Texts[0] is NOT Hit Text!");
+
+
+        //foreach (var text in Texts)
+        //{
+        //    if (text.CompareTag("HitText"))
+        //    {
+        //        text.GetComponent<TextMesh>().text = "Hits : " + HitCounts.ToString();
+        //        return;
+        //    }
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        foreach (var text in Texts)
+        {
+            text.transform.LookAt(HMD.transform.position);
+        }
+    }
+
+    public void AddHitCount()
+    {
+        HitCounts++;
+        UpdateText();
+    }
+
+    void UpdateText()
+    {
+        Texts[1].GetComponent<TextMesh>().text = "Hits : " + HitCounts.ToString();
+    }
+}
